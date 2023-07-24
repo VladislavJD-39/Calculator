c7ce0bac5323287f667c50b3dfaebeacd0b397f4
Vladislav JD-39 <vladislav.zayatdinov@yandex.ru>
2023-07-24 23:20:42 +0400

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Ints intsCalc = new IntsCalculator();
-        System.out.println(intsCalc.sum(2, 2));
-        System.out.println(intsCalc.sum(10, 22));
-        System.out.println(intsCalc.pow(2, 10));
+        System.out.println(intsCalc.sum(1, 2));
+        System.out.println(intsCalc.sum(11, 22));
+        System.out.println(intsCalc.pow(10, 78));
     }
 }
